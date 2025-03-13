6c1b4fdce42b09dc58d29f69165d4801420c7b1a
Flazer <146836746+Flazer0136@users.noreply.github.com>
2025-03-12 22:59:15 -0600

    initial commit

---
+++ TripLogger/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TripLogger.Models;
+
+namespace TripLogger.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Repository<Trip> data { get; set; }
+        public HomeController(TripLoggerContext ctx) => data = new Repository<Trip>(ctx);
+
+        public ViewResult Index()
+        {
+            var options = new QueryOptions<Trip> { 
+                Includes = "Destination, Accommodation, TripActivities.Activity",
+                OrderBy = t => t.StartDate
+            };
+
+            var trips = data.List(options);
+            return View(trips);
+        }
+
+    }
+}
---
+++ TripLogger/Models/ViewModels/DropDownViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TripLogger.Models
+{
+    public class DropDownViewModel
+    {
+        public IEnumerable<Destination> Destinations { get; set; }
+        public IEnumerable<Accommodation> Accommodations { get; set; }
+        public IEnumerable<Activity> Activities { get; set; }
+    }
+}
